7be84727f16d1f5d2735d4465cfea1e905a85e8b
Maxime Boulin <boulinm@amazon.com>
2021-06-16 08:15:22 -0700

    cleanup and fixes

--- deployment/setup-lambdas.sh
+++ deployment/setup-lambdas.sh
@@ -5,7 +5,13 @@ printf "\nGenerating Lambda functions zip files...\n"
 node zip-generator.js ../serverless/lambda-on-connect ../serverless/lambda-on-disconnect ../serverless/lambda-send-transcription
 
 printf "\nCreating S3 bucket to upload Lambda functions zip files...\n"
-aws s3api create-bucket --bucket ivs-transcribe-demo-lambda-functions-<RANDOM_SUFFIX> --region ${region} --create-bucket-configuration LocationConstraint=${region}
+if [ ${region} == 'us-east-1' ]
+then
+    aws s3api create-bucket --bucket ivs-transcribe-demo-lambda-functions-<RANDOM_SUFFIX> --region ${region}
+else
+    aws s3api create-bucket --bucket ivs-transcribe-demo-lambda-functions-<RANDOM_SUFFIX> --region ${region} --create-bucket-configuration LocationConstraint=${region}
+fi
+
 if [ $? != 0 ]; then exit 1; fi
 
 printf "\nUploading Lambda functions zip files into S3 bucket...\n"
